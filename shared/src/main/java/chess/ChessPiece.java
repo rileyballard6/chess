a9ee24d3ce1e63c2ef3acb7b6c3c09cc834fa00e
rileyballard6 <rileyrb01@gmail.com>
2024-09-17 15:22:16 -0600

    refactored code, now there is one function for every piece and parameters taken are directions and whether or not it can make multiple moves

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -56,100 +56,53 @@ public class ChessPiece {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         Collection<ChessMove> validMoves = new ArrayList<>();
         switch (type) {
-            case ROOK -> validMoves = getValidMovesRook(board, myPosition);
-            case KING -> validMoves = getValidMovesKing(board, myPosition);
-            case BISHOP -> validMoves = getValidMovesBishop(board, myPosition);
-            case QUEEN -> validMoves = getValidMovesQueen(board, myPosition);
+            case ROOK -> validMoves = getValidMoves(board, myPosition, new int[][] {
+                    {-1, 0},
+                    {1, 0},
+                    {0, 1},
+                    {0, -1}
+            }, true);
+            case KING -> validMoves = getValidMoves(board, myPosition, new int[][] {
+                    {-1,0},
+                    {1,0},
+                    {0,1},
+                    {0,-1},
+                    {-1,-1},
+                    {1,-1},
+                    {-1,1},
+                    {1,1},
+            }, false );
+            case BISHOP -> validMoves = getValidMoves(board, myPosition, new int[][] {
+                    {-1,-1},
+                    {1,-1},
+                    {-1,1},
+                    {1,1},
+            }, true);
+            case QUEEN -> validMoves = getValidMoves(board, myPosition, new int[][] {
+                    {-1,0},
+                    {1,0},
+                    {0,1},
+                    {0,-1},
+                    {-1,-1},
+                    {1,-1},
+                    {-1,1},
+                    {1,1},
+            }, true);
         }
         return validMoves;
     }
 
-    private Collection<ChessMove> getValidMovesQueen(ChessBoard board, ChessPosition myPosition) {
+    private Collection<ChessMove> getValidMoves(ChessBoard board, ChessPosition myPosition, int[][] directions, boolean loop) {
         Collection<ChessMove> validMoves = new ArrayList<>();
         int column = myPosition.getColumn();
         int row = myPosition.getRow();
 
-        int[][] directions = {
-                {-1,0},
-                {1,0},
-                {0,1},
-                {0,-1},
-                {-1,-1},
-                {1,-1},
-                {-1,1},
-                {1,1},
-        };
-
-        pieceLoopMultipleMoves(board, myPosition, row, column, directions, validMoves);
-        return validMoves;
-    }
-
-    private Collection<ChessMove> getValidMovesBishop(ChessBoard board, ChessPosition myPosition) {
-        Collection<ChessMove> validMoves = new ArrayList<>();
-        int column = myPosition.getColumn();
-        int row = myPosition.getRow();
-
-        int[][] directions = {
-                {-1,-1},
-                {1,-1},
-                {-1,1},
-                {1,1},
-        };
-
-        pieceLoopMultipleMoves(board, myPosition, row, column, directions, validMoves);
-        return validMoves;
-    }
-
-    private Collection<ChessMove> getValidMovesKing(ChessBoard board, ChessPosition myPosition) {
-        Collection<ChessMove> validMoves = new ArrayList<>();
-        int column = myPosition.getColumn();
-        int row = myPosition.getRow();
-
-        int[][] directions = {
-                {-1,0},
-                {1,0},
-                {0,1},
-                {0,-1},
-                {-1,-1},
-                {1,-1},
-                {-1,1},
-                {1,1},
-        };
-
-        for (int[] direction : directions) {
-            int rowOffset = direction[0];
-            int colOffset = direction[1];
-            int newRow = row + rowOffset;
-            int newCol = column + colOffset;
-
-            if (newRow <= 0 || newRow > 8 || newCol <= 0 || newCol > 8) break;
-            ChessPosition new_position = new ChessPosition(newRow, newCol);
-
-            if (board.getPiece(new_position) == null) {
-                validMoves.add(new ChessMove(myPosition, new_position, null));
-            } else if (board.getPiece(new_position).getTeamColor() == pieceColor) {
-                System.out.println("Teammate here. Cannot move.");
-            } else {
-                System.out.println("Piece captured!");
-                validMoves.add(new ChessMove(myPosition, new_position, null));
-            }
-
+        if (loop) {
+            pieceLoopMultipleMoves(board, myPosition, row, column, directions, validMoves);
+        } else {
+            pieceLoopOneMove(board, myPosition, row, column, directions, validMoves);
         }
-        return validMoves;
-    }
 
-    private Collection<ChessMove> getValidMovesRook(ChessBoard board, ChessPosition myPosition) {
-        Collection<ChessMove> validMoves = new ArrayList<>();
-        int column = myPosition.getColumn();
-        int row = myPosition.getRow();
-
-        int[][] directions = {
-                {-1, 0},
-                {1, 0},
-                {0, 1},
-                {0, -1}
-        };
-        pieceLoopMultipleMoves(board, myPosition, row, column, directions, validMoves);
         return validMoves;
     }
 
@@ -179,6 +132,28 @@ public class ChessPiece {
                     break;
                 }
             }
+        }
+    }
+
+    private void pieceLoopOneMove(ChessBoard board, ChessPosition myPosition, int row, int column, int[][] directions, Collection<ChessMove> validMoves) {
+        for (int[] direction : directions) {
+            int rowOffset = direction[0];
+            int colOffset = direction[1];
+            int newRow = row + rowOffset;
+            int newCol = column + colOffset;
+
+            if (newRow <= 0 || newRow > 8 || newCol <= 0 || newCol > 8) break;
+            ChessPosition new_position = new ChessPosition(newRow, newCol);
+
+            if (board.getPiece(new_position) == null) {
+                validMoves.add(new ChessMove(myPosition, new_position, null));
+            } else if (board.getPiece(new_position).getTeamColor() == pieceColor) {
+                System.out.println("Teammate here. Cannot move.");
+            } else {
+                System.out.println("Piece captured!");
+                validMoves.add(new ChessMove(myPosition, new_position, null));
+            }
+
         }
     }
 
