70aa08bf403058012cfa57ecb7ed2136f3ec1bca
rileyballard6 <rileyrb01@gmail.com>
2024-11-14 08:47:32 -0700

    register test added

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,46 @@
 package client;
 
+import facade.ServerFacade;
+import model.UserData;
 import org.junit.jupiter.api.*;
 import server.Server;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade serverFacade;
 
+    static {
+        try {
+            serverFacade = new ServerFacade();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //Set up should clear the database each time.
     @BeforeAll
-    public static void init() {
+    public static void init() throws IOException {
         server = new Server();
-        var port = server.run(0);
+        var port = server.run(8080);
         System.out.println("Started test HTTP server on " + port);
+
+        URL url = new URL("http://localhost:8080/db");
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        http.setRequestMethod("DELETE");
+        http.setRequestProperty("Content-Type", "application/json; utf-8");
+
+        int responseCode = http.getResponseCode();
+        System.out.println("Response Code: " + responseCode);
+
     }
 
     @AfterAll
@@ -20,49 +48,69 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @Test
+    public void registerTestTrue() throws Exception {
+        UserData registerTest = new UserData("testuser", "passwordtest", "email@gmail.com");
+
+        String answer = serverFacade.registerCall(registerTest);
+        assertTrue(answer.length() > 10);
+    }
+
+    @Test
+    public void registerTestFalse() throws Exception {
+        UserData registerTest = new UserData("testuser", "passwordtest", "email@gmail.com");
+
+        String answer = serverFacade.registerCall(registerTest);
+        assertFalse(answer.isEmpty());
+    }
+
+    @Test
+    public void loginTestTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void loginTestFalse() {
+        assertTrue(true);
+    }
+
+
 
     @Test
     public void logoutTest() {
-        Assertions.assertTrue(true);
+        assertTrue(true);
     }
 
-    @Test
-    public void loginTest() {
-        Assertions.assertTrue(true);
-    }
 
-    @Test
-    public void registerTest() {
-        Assertions.assertTrue(true);
-    }
+
 
     @Test
     public void joinGameTest() {
-        Assertions.assertTrue(true);
+        assertTrue(true);
     }
     @Test
     public void listGameTest() {
-        Assertions.assertTrue(true);
+        assertTrue(true);
     }
 
     @Test
     public void createGameTest() {
-        Assertions.assertTrue(true);
+        assertTrue(true);
     }
 
     @Test
     public void observeGameTest() {
-        Assertions.assertTrue(true);
+        assertTrue(true);
     }
 
     @Test
     public void makeRequestTest() {
-        Assertions.assertTrue(true);
+        assertTrue(true);
     }
 
     @Test
     public void makePostRequestTest() {
-        Assertions.assertTrue(true);
+        assertTrue(true);
     }
 
 }
