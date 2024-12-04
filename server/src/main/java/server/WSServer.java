7ac89612ebe84a8189fc5ce067126b54d795b368
rileyballard6 <rileyrb01@gmail.com>
2024-12-03 18:57:33 -0700

    keeping track of sessions now to properly alert players

--- server/src/main/java/server/WSServer.java
+++ server/src/main/java/server/WSServer.java
@@ -6,8 +6,14 @@ import spark.*;
 import org.eclipse.jetty.websocket.api.annotations.*;
 import websocket.messages.ServerMessage;
 
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
 @WebSocket
 public class WSServer {
+    private static final Set<Session> activeSessions = new CopyOnWriteArraySet<>();
+
+
     public static void main(String[] args) {
         new Server().run(8080);
     }
@@ -24,16 +30,34 @@ public class WSServer {
         return Spark.port();
     }
 
+
+    @OnWebSocketConnect
+    public void onConnect(Session session) {
+        activeSessions.add(session);
+        System.out.println("New connection added");
+    }
+
+    @OnWebSocketClose
+    public void onClose(Session session, int statusCode, String reason) {
+        activeSessions.remove(session);
+        System.out.println("Connection closed");
+    }
+
     @OnWebSocketMessage
     public void onMessage(Session session, String message) throws Exception {
         var body = getBody(message, websocket.commands.UserGameCommand.class);
-        System.out.println(body.getCommandType());
 
         switch (body.getCommandType()) {
             case CONNECT -> {
                 ServerMessage newMessage = new ServerMessage(ServerMessage.ServerMessageType.LOAD_GAME, "Game loaded");
                 session.getRemote().sendString(new Gson().toJson(newMessage));
-                session.getRemote().flush();
+
+                ServerMessage joinMessage = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION, "A new player has joined");
+                for (Session activeSession : activeSessions) {
+                    if (!activeSession.equals(session) && activeSession.isOpen()) {
+                        activeSession.getRemote().sendString(new Gson().toJson(joinMessage));
+                    }
+                }
             }
             case MAKE_MOVE, RESIGN, LEAVE -> {
                 ServerMessage newMessage = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION, "This is a notification");
